b6eac2153f2a9efcaecc0dca32b3aa2ef7fe8eec
星野龍 <ryu0114h@gmail.com>
2022-04-21 07:47:44 +0900

    [fix] customizing the multiple users

--- src/main/java/com/example/expensetrackerapi/config/WebSecurityConfig.java
+++ src/main/java/com/example/expensetrackerapi/config/WebSecurityConfig.java
@@ -1,8 +1,10 @@
 package com.example.expensetrackerapi.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -15,5 +17,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("test1").password("password").authorities("admin")
+                .and()
+                .withUser("test2").password("password").authorities("user")
+                .and()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 }
