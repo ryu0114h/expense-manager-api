2bcae664fd9a6ba3edb249f6188888baeaa8cc7c
星野龍 <ryu0114h@gmail.com>
2022-04-21 07:40:06 +0900

    [add] setup spring security

---
+++ src/main/java/com/example/expensetrackerapi/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.example.expensetrackerapi.controller;
+
+import com.example.expensetrackerapi.entity.User;
+import com.example.expensetrackerapi.entity.UserModel;
+import com.example.expensetrackerapi.service.UserService;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> save(@Valid @RequestBody UserModel user) {
+        return new ResponseEntity<User>(userService.createUser(user), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login() {
+        return new ResponseEntity<String>("User is logged in", HttpStatus.OK);
+    }
+}
--- src/main/java/com/example/expensetrackerapi/controller/UserController.java
+++ src/main/java/com/example/expensetrackerapi/controller/UserController.java
@@ -3,14 +3,12 @@ package com.example.expensetrackerapi.controller;
 import com.example.expensetrackerapi.entity.User;
 import com.example.expensetrackerapi.entity.UserModel;
 import com.example.expensetrackerapi.service.UserService;
-import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,11 +18,6 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-
-    @PostMapping("/register")
-    public ResponseEntity<User> save(@Valid @RequestBody UserModel user) {
-        return new ResponseEntity<User>(userService.createUser(user), HttpStatus.CREATED);
-    }
 
     @GetMapping("/users/{id}")
     public ResponseEntity<User> readUser(@PathVariable Long id) {
