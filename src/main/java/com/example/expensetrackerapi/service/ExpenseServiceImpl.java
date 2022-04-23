b4b583a04d88c90bfaec1a35552b9269e039e573
星野龍 <ryu0114h@gmail.com>
2022-04-24 03:00:01 +0900

    [fix] Update the finder methods for logged in user

--- src/main/java/com/example/expensetrackerapi/service/ExpenseServiceImpl.java
+++ src/main/java/com/example/expensetrackerapi/service/ExpenseServiceImpl.java
@@ -62,12 +62,14 @@ public class ExpenseServiceImpl implements ExpenseService {
 
     @Override
     public List<Expense> readByCategory(String category, Pageable page) {
-        return expenseRepository.findByCategory(category, page).toList();
+        return expenseRepository.findByUserIdAndCategory(userService.getLoggedInUser().getId(), category, page)
+                .toList();
     }
 
     @Override
     public List<Expense> readByName(String keyword, Pageable page) {
-        return expenseRepository.findByNameContaining(keyword, page).toList();
+        return expenseRepository.findByUserIdAndNameContaining(userService.getLoggedInUser().getId(), keyword, page)
+                .toList();
     }
 
     @Override
@@ -78,6 +80,7 @@ public class ExpenseServiceImpl implements ExpenseService {
         if (endDate == null) {
             endDate = new Date(System.currentTimeMillis());
         }
-        return expenseRepository.findByDateBetween(startDate, endDate, page).toList();
+        return expenseRepository.findByUserIdAndDateBetween(userService.getLoggedInUser().getId(), startDate, endDate,
+                page).toList();
     }
 }
